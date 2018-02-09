9743792a7df873c25a737404ed3b71955ba371aa
Karina Tiurina <rktiurina@gmail.com>
2018-02-09 10:29:24 +0300

    backend and frontend in different folders

---
+++ backend/app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def home
+    message = "Hello from rails server!"
+    respond_to do |format|
+      format.json {render json: {:success => true, :message => message}, status: 200}
+    end
+  end
+
+  def sports
+    message = "This is message from sports action!"
+    respond_to do |format|
+      format.json {render json: {:success => true, :message => message}, status: 200}
+    end
+  end
+end
---
+++ backend/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "application#home", defaults: { format: 'json' }
+
+  get "home" => "application#home", defaults: { format: 'json' }
+  get "sports" => "application#sports", defaults: { format: 'json' }
+end
